f73587fa8e857f273c5bb5435edbadbb2261408b
Istvan Szonyi <pisti.szonyi@gmail.com>
2015-04-13 16:03:42 +0200

    Create dflop.vhd

---
+++ dflop.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:52:42 04/13/2015 
+-- Design Name: 
+-- Module Name:    dflop - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity dflop is
+	port(clk, data : in std_logic;
+	Q : out std_logic);
+end dflop;
+
+architecture Behavioral of dflop is
+
+begin
+	process(clk)
+	begin
+		if (clk'event and clk='1') then
+			Q <= data;
+		end if;
+	end process;
+end Behavioral;
+
