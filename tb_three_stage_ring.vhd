35fd60eacf098c03ee0aa74de0ba82d1099c2d7e
russellbarnes <russellbarnes@users.noreply.github.com>
2015-04-24 11:51:59 +0200

    Separate init signals for third click element

--- tb_three_stage_ring.vhd
+++ tb_three_stage_ring.vhd
@@ -17,7 +17,7 @@ architecture testbench of tb_three_stage_ring is
 
 component three_stage_ring
 port(
-	init, init_clk : in std_logic;
+	init, init_clk, set_3, reset_3, init_clk_3 : in std_logic;
 	output : out std_logic
 );
 end component;
@@ -25,31 +25,56 @@ end component;
 signal init_s : std_logic;
 signal init_clk_s : std_logic;
 signal output_s : std_logic;
+signal set_3_s : std_logic;
+signal reset_3_s : std_logic;
+signal init_clk_3_s : std_logic;
 
 begin
 
 dut : three_stage_ring port map(
 	init => init_s,
 	init_clk => init_clk_s,
+	set_3 => set_3_s,
+	reset_3 => reset_3_s,
+	init_clk_3 => init_clk_3_s,
 	output => output_s
 );
 
 process
 begin
 
+-- Initialize all three click elements to 0
 init_clk_s <='0';
 init_s <= '0';
+set_3_s <= '0';
+reset_3_s <= '0';
+init_clk_3_s <= '0';
 wait for 5 ns;
 init_s <= '1';
+reset_3_s <= '1';
 wait for 10 ns;
 init_clk_s <= '1';
+init_clk_3_s <= '1';
 wait for 5 ns;
 init_s <= '0';
+reset_3_s <= '0';
 init_clk_s <= '0';
-wait for 120 ns;
+init_clk_3_s <= '0';
+wait for 35 ns;
+
+-- Fire request on the third click element
+set_3_s <= '1';
+wait for 35 ns;
+init_clk_3_s <= '1';
+wait for 5 ns;
+init_clk_3_s <= '0';
+set_3_s <= '0';
+
+wait for 200 ns;
 
 end process;
 	   
   
 
 end testbench;
+
