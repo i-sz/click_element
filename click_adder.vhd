abf08aa274c91a169eabde1d6baf0c9423aa5cf1
russellbarnes <russellbarnes@users.noreply.github.com>
2015-05-17 16:16:27 +0200

    Timing fix and 8-bit data

--- click_adder.vhd
+++ click_adder.vhd
@@ -19,6 +19,8 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -30,8 +32,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity click_adder is
-	port(a_req, b_req, c_ack, a_data, b_data, reset, init_clk : in std_logic;
-	a_ack, b_ack, c_req, c_data : out std_logic);
+	port(a_req, b_req, c_ack, reset, init_clk : in std_logic; 
+	a_data, b_data : in std_logic_vector(7 downto 0);	 
+	a_ack, b_ack, c_req : out std_logic;
+	c_data : out std_logic_vector(7 downto 0)
+	);
 end click_adder;
 
 architecture Behavioral of click_adder is
@@ -41,9 +46,12 @@ component adder_combo
 	combo_out : out std_logic);
 end component;
 
-component dflop
-	port(clk, data, reset, set : in std_logic;
-	Q : out std_logic);
+
+component dflop_ctrl is
+	port(clk, reset, set : in std_logic; 
+	data : in std_logic; 
+	Q : out std_logic
+	);
 end component;
 
 signal c_req_internal : std_logic;
@@ -55,10 +63,11 @@ begin
 
 not_c_req_internal <= not c_req_internal;
 c_req <= c_req_internal after 8 ns;
-a_ack <= c_req_internal;
-b_ack <= c_req_internal;
+-- Assigning a_ack and b_ack with the same delay to give the next Click element a chance to collect the data
+a_ack <= c_req_internal after 8 ns;
+b_ack <= c_req_internal after 8 ns;
 
-c_data <= a_data or b_data; -- Will change to addition after expanding to bus
+c_data <= a_data + b_data;
 
 combo_i : adder_combo port map (
 	a_req => a_req,
@@ -69,7 +78,7 @@ combo_i : adder_combo port map (
 	combo_out => combo_out_internal
 	);
 
-dflop_ctl : dflop port map (
+dflop_ctl : dflop_ctrl port map (
 	clk => combo_out_internal,
 	data => not_c_req_internal,
 	Q => c_req_internal,
@@ -78,5 +87,3 @@ dflop_ctl : dflop port map (
 	);
 
 end Behavioral;
-
-
