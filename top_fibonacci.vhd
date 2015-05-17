12a4977ec87c2adaf5aee9a56af67c5b1dc80ca7
russellbarnes <russellbarnes@users.noreply.github.com>
2015-05-17 16:22:48 +0200

    Update top_fibonacci.vhd

--- top_fibonacci.vhd
+++ top_fibonacci.vhd
@@ -18,40 +18,46 @@ architecture testbench of top_fibonacci is
 component three_stage_ring_fb is
 port(
 	init, init_clk, set_3, reset_3, init_clk_3 : in std_logic;
-	a_req_i, b_ack_i, data_i : in std_logic;
-	a_ack_o, b_req_o, data_o : out std_logic
+	a_req_i, b_ack_i : in std_logic;
+	data_i : in std_logic_vector(7 downto 0);
+	a_ack_o, b_req_o : out std_logic; 
+	data_o : out std_logic_vector(7 downto 0)
 );
 end component;
 
-component fork_click_pipeline
-	port(a_req, b_ack1, b_ack2, a_data, reset, init_clk : in std_logic;
-	a_ack, b_req1, b_req2, b_data1, b_data2 : out std_logic);
+component three_stage_ringA_fb is
+port(
+	init, init_clk, set_3, reset_3, init_clk_3, b_ack1, b_ack2 : in std_logic;
+	a_req_i : in std_logic;
+	data_i : in std_logic_vector(7 downto 0);
+	a_ack_o, b_req1, b_req2 : out std_logic; 
+	b_data1, b_data2 : out std_logic_vector(7 downto 0)
+);
 end component;
 
 component click_adder
-	port(a_req, b_req, c_ack, a_data, b_data, reset, init_clk : in std_logic;
-	a_ack, b_ack, c_req, c_data : out std_logic);
+	port(a_req, b_req, c_ack, reset, init_clk : in std_logic;
+	a_data, b_data : in std_logic_vector(7 downto 0);
+	a_ack, b_ack, c_req : out std_logic; 
+	c_data : out std_logic_vector(7 downto 0)
+	);
 end component;
 
 
 signal init_s : std_logic;
 signal init_clk_s : std_logic;
-signal output_s : std_logic;
 signal set_3_s : std_logic;
 signal reset_3_s : std_logic;
 signal init_clk_3_s : std_logic;
 
-signal data_1 : std_logic;
-signal data_2 : std_logic;
-signal data_3 : std_logic;
-signal data_4 : std_logic;
-signal data_5 : std_logic;
-signal ack_1 : std_logic;
+signal data_2 : std_logic_vector(7 downto 0);
+signal data_3 : std_logic_vector(7 downto 0);
+signal data_4 : std_logic_vector(7 downto 0);
+signal data_5 : std_logic_vector(7 downto 0);
 signal ack_2 : std_logic;
 signal ack_3 : std_logic;
 signal ack_4 : std_logic;
 signal ack_5 : std_logic;
-signal req_1 : std_logic;
 signal req_2 : std_logic;
 signal req_3 : std_logic;
 signal req_4 : std_logic;
@@ -60,13 +66,16 @@ signal req_5 : std_logic;
 begin
 
 -- First stage of three click elements
-ringA : three_stage_ring_fb port map(
+ringA : three_stage_ringA_fb port map(
 	a_req_i => req_5, 
-	b_ack_i => ack_1, 
+	b_ack1 => ack_2,
+	b_ack2 => ack_3,
 	data_i => data_5,
 	a_ack_o => ack_5, 
-	b_req_o => req_1, 
-	data_o => data_1, 
+	b_req1 => req_2, 
+	b_req2 => req_3, 
+	b_data1 => data_2, 
+	b_data2 => data_3,
 	init => init_s,
 	init_clk => init_clk_s,
 	set_3 => set_3_s,
@@ -87,21 +96,6 @@ ringB : three_stage_ring_fb port map(
 	set_3 => set_3_s,
 	reset_3 => reset_3_s,
 	init_clk_3 => init_clk_3_s
-);
-
--- Fork from first stage of click elements to second stage of click elements AND adder
-fork : fork_click_pipeline port map(
-	a_req => req_1, 
-	b_ack1 => req_2, 
-	b_ack2 => req_3, 
-	a_data => data_1, 
-	reset => reset_3_s, 
-	init_clk => init_clk_s, 
-	a_ack => ack_1, 
-	b_req1 => req_2, 
-	b_req2 => req_3, 
-	b_data1 => data_2, 
-	b_data2 => data_3 
 );
 
 -- Accept input from fork and second stage of click elements, and JOIN to first stage of click elements
@@ -146,14 +140,11 @@ set_3_s <= '1';
 wait for 35 ns;
 init_clk_3_s <= '1';
 wait for 5 ns;
-init_clk_3_s <= '0';
 set_3_s <= '0';
+init_clk_3_s <= '0';
 
-wait for 200 ns;
+
+wait for 1000 ns;
 
 end process;
-	   
-  
-
 end testbench;
-
